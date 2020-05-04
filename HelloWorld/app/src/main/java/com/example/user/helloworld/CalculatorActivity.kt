ac7b5b907256e581c9aca73ec2c4bd2f533559bd
Arturo Sanchez <ascstb@gmail.com>
2020-05-04 18:46:47 -0500

    Add onNumberButtonClicked, Timber Dependency, Application in Manifest and Class, plant Timber on MyApp

--- HelloWorld/app/src/main/java/com/example/user/helloworld/CalculatorActivity.kt
+++ HelloWorld/app/src/main/java/com/example/user/helloworld/CalculatorActivity.kt
@@ -2,11 +2,17 @@ package com.example.user.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import timber.log.Timber
 
 class CalculatorActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calculator_constraint_layout)
+    }
+
+    fun onNumberButtonClicked(view: View) {
+        Timber.d("CalculatorActivity_TAG: onNumberButtonClicked: ${view.id}")
     }
 }

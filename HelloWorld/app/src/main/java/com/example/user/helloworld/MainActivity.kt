9d2eaa088ee929b8862e717548629c416600a849
mac-asanchez <ascstb@gmail.com>
2020-05-11 19:17:14 -0500

    Activity for result

--- HelloWorld/app/src/main/java/com/example/user/helloworld/MainActivity.kt
+++ HelloWorld/app/src/main/java/com/example/user/helloworld/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,7 +14,19 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun calculatorButtonClick(view: View) {
+        Timber.d("MainActivity_TAG: calculatorButtonClick: ")
         val intent = Intent(this, CalculatorActivity::class.java)
         startActivity(intent)
+    }
+
+    fun activityForResultButtonClick(view: View) {
+        Timber.d("MainActivity_TAG: activityForResultButtonClick: ")
+        val intent = Intent(this, CalculatorActivity::class.java)
+        startActivityForResult(intent, 1000)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        Timber.d("MainActivity_TAG: onActivityResult: requestCode: $requestCode, resultCode: $resultCode, data: $data")
     }
 }

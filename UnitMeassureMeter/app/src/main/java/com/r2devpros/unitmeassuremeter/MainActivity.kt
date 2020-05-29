3bc7d2c87c1284b65d3f0fd8fad82d9d268105df
Arturo S <ascstb@gmail.com>
2020-05-29 14:12:06 -0500

    SeekBar implementation

--- UnitMeassureMeter/app/src/main/java/com/r2devpros/unitmeassuremeter/MainActivity.kt
+++ UnitMeassureMeter/app/src/main/java/com/r2devpros/unitmeassuremeter/MainActivity.kt
@@ -1,12 +1,51 @@
 package com.r2devpros.unitmeassuremeter
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.EditText
+import android.widget.SeekBar
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private var skSize: SeekBar? = null
+    private var etSize: EditText? = null
+    private var tvResult: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        bindViews()
+    }
+
+    private fun bindViews() {
+        Log.d("MainActivity_TAG", "bindViews: ")
+        skSize = findViewById(R.id.skSize)
+        etSize = findViewById(R.id.etSize)
+        tvResult = findViewById(R.id.tvResult)
+
+        skSize?.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                Log.d("MainActivity_TAG", "onProgressChanged: ")
+                etSize?.setText(progress.toString())
+
+                //default usa SP
+                tvResult?.textSize = progress.toFloat()
+//                tvResult?.setTextSize(TypedValue.COMPLEX_UNIT_PX, 0f)
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                Log.d("MainActivity_TAG", "onStartTrackingTouch: ")
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                Log.d("MainActivity_TAG", "onStopTrackingTouch: ")
+            }
+        })
     }
 }

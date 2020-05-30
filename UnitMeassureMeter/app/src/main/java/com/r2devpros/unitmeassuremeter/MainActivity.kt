0f45b8115ce90c022ad559d578f14854f872aa2f
chrisalzam <chdsz123@hotmail.com>
2020-05-30 01:19:55 -0500

    TextWatcher in etSize

--- UnitMeassureMeter/app/src/main/java/com/r2devpros/unitmeassuremeter/MainActivity.kt
+++ UnitMeassureMeter/app/src/main/java/com/r2devpros/unitmeassuremeter/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.r2devpros.unitmeassuremeter
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.widget.EditText
 import android.widget.SeekBar
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -15,6 +18,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        title = "Main"
     }
 
     override fun onResume() {
@@ -32,11 +37,7 @@ class MainActivity : AppCompatActivity() {
         skSize?.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
                 Log.d("MainActivity_TAG", "onProgressChanged: ")
-                etSize?.setText(progress.toString())
 
-                //default usa SP
-                tvResult?.textSize = progress.toFloat()
-//                tvResult?.setTextSize(TypedValue.COMPLEX_UNIT_PX, 0f)
             }
 
             override fun onStartTrackingTouch(seekBar: SeekBar?) {
@@ -44,8 +45,38 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onStopTrackingTouch(seekBar: SeekBar?) {
-                Log.d("MainActivity_TAG", "onStopTrackingTouch: ")
+                Log.d("MainActivity_TAG", "onStopTrackingTouch: ${seekBar?.progress.toString()}")
+                val progressval = seekBar?.progress.toString()
+                etSize?.setText(progressval)
+
+                //default usa SP
+                tvResult?.textSize = progressval.toFloat()
+//                tvResult?.setTextSize(TypedValue.COMPLEX_UNIT_PX, 0f)
             }
         })
+
+
+        etSize?.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {
+                Log.d("MainActivity_TAG", "afterTextChanged: $p0")
+                skSize?.progress = p0.toString().toInt()
+                tvResult?.textSize = p0.toString().toFloat()
+                if (p0.toString() == "")
+                    Log.d("MainActivity_TAG", "etSize value is null")
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                Log.d("MainActivity_TAG", "beforeTextChanged: $p0")
+
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                Log.d("MainActivity_TAG", "onTextChanged: $p0")
+
+            }
+        })
+
+
     }
 }
+

3f250eb50ad1edb8324e3ceb38652bcb3253cd73
Arturo S <ascstb@gmail.com>
2020-06-01 18:46:20 -0500

    Updating Main Activity Layout

--- UnitMeassureMeter/app/src/main/java/com/r2devpros/unitmeassuremeter/MainActivity.kt
+++ UnitMeassureMeter/app/src/main/java/com/r2devpros/unitmeassuremeter/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.r2devpros.unitmeassuremeter
 
-import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.text.Editable
@@ -11,16 +10,8 @@ import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 
-const val CONVERTER_FIRST_KEY="CONVERTER KEY"
-const val CONVERTER_WIDTH_PIXEL_KEY="WIDTHPX"
-const val CONVERTER_HEIGHT_PIXEL_KEY="HEIGHTPX"
-const val CONVERTER_WIDTH_DP_KEY="WIDTHDP"
-const val CONVERTER_HEIGHT_DP_KEY="HEIGHTDP"
-const val CONVERTER_DPI="DPI"
-const val ISTEXTVIEW = "ISATEXTVIEW"
-
-
 class MainActivity : AppCompatActivity() {
+    //region views
     private var skSize: SeekBar? = null
     private var etSize: EditText? = null
     private var skButtonSize: SeekBar? = null
@@ -32,59 +23,27 @@ class MainActivity : AppCompatActivity() {
     private var tvWidthDp: TextView? = null
     private var tvHeightPx: TextView? = null
     private var tvHeightDp: TextView? = null
+    //endregion
+
+    //region variables
     private var width = 0
     private var height = 0
     private var widthDP = 0
     private var heightDP = 0
     private var dpi = 0
+    //endregion
 
+    //region life cycle
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        title = "Main"
+        title = getString(R.string.app_name)
 
-        // get device dimensions
-        val displayMetrics = DisplayMetrics()
-        windowManager.defaultDisplay.getMetrics(displayMetrics)
-
-        width = displayMetrics.widthPixels
-        height = displayMetrics.heightPixels
-
+        getDeviceDimension()
         bindScreenSizeViews()
 
-        tvWidthPx?.text = width.toString()
-        tvHeightPx?.text = height.toString()
-
-
-        dpi = getDensity(this)
-
-
-        widthDP = width * 160 / dpi
-        heightDP = height * 160 / dpi
-
-        tvWidthDp?.text = widthDP.toString()
-        tvHeightDp?.text = heightDP.toString()
-    }
-
-    private fun getDensity(context: Context): Int {
-        val density: Float = context.getResources().getDisplayMetrics().density
- //       Log.d("Density", "${density.toString()}")
-        return when (density) {
-            4f -> 640
-            3f -> 480
-            2f -> 320
-            1.5f -> 240
-            1.0f -> 160
-            else -> 120
-        }
-    }
-
-    private fun bindScreenSizeViews() {
-        tvWidthPx = findViewById(R.id.tvTotalW_ValuePX)
-        tvWidthDp = findViewById(R.id.tvTotalW_ValueDP)
-        tvHeightPx = findViewById(R.id.tvTotalH_ValuePX)
-        tvHeightDp = findViewById(R.id.tvTotalH_ValueDP)
+        initValues()
     }
 
     override fun onResume() {
@@ -93,13 +52,50 @@ class MainActivity : AppCompatActivity() {
         bindTextViews()
         bindButtonViews()
     }
+    //endregion
+
+    //region functions
+    private fun initValues() {
+        tvWidthPx?.text = width.toString()
+        tvHeightPx?.text = height.toString()
+
+        dpi = getDensity()
+
+        widthDP = width * 160 / dpi
+        heightDP = height * 160 / dpi
+
+        tvWidthDp?.text = widthDP.toString()
+        tvHeightDp?.text = heightDP.toString()
+    }
+
+    private fun getDeviceDimension() {
+        val displayMetrics = DisplayMetrics()
+        windowManager.defaultDisplay.getMetrics(displayMetrics)
+
+        width = displayMetrics.widthPixels
+        height = displayMetrics.heightPixels
+    }
+
+    private fun getDensity(): Int =
+        when (resources.displayMetrics.density) {
+            4f -> 640
+            3f -> 480
+            2f -> 320
+            1.5f -> 240
+            1.0f -> 160
+            else -> 120
+        }
+
+    private fun bindScreenSizeViews() {
+        tvWidthPx = findViewById(R.id.tvTotalW_ValuePX)
+        tvWidthDp = findViewById(R.id.tvTotalW_ValueDP)
+        tvHeightPx = findViewById(R.id.tvTotalH_ValuePX)
+        tvHeightDp = findViewById(R.id.tvTotalH_ValueDP)
+    }
 
     private fun bindTextViews() {
         Log.d("MainActivity_TAG", "bindTextViews: ")
-        skSize = findViewById(R.id.skSize)
-        etSize = findViewById(R.id.etSize)
         tvResult = findViewById(R.id.tvResult)
-        spTextType = findViewById(R.id.spSizeType)
 
         ArrayAdapter.createFromResource(
             this,
@@ -137,7 +133,6 @@ class MainActivity : AppCompatActivity() {
             }
         })
 
-
         etSize?.addTextChangedListener(object : TextWatcher {
             var value = ""
             override fun afterTextChanged(p0: Editable?) {
@@ -159,14 +154,10 @@ class MainActivity : AppCompatActivity() {
 
             }
         })
-
-
     }
 
     private fun bindButtonViews() {
         Log.d("MainActivity_TAG", "bindButtonViews: ")
-        skButtonSize = findViewById(R.id.skButtonSize)
-        etButtonSize = findViewById(R.id.etButtonSize)
         btnText = findViewById(R.id.btn_Text)
 
         skButtonSize?.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
@@ -194,7 +185,6 @@ class MainActivity : AppCompatActivity() {
             }
         })
 
-
         etButtonSize?.addTextChangedListener(object : TextWatcher {
             var value = ""
             override fun afterTextChanged(p0: Editable?) {
@@ -218,12 +208,26 @@ class MainActivity : AppCompatActivity() {
         })
     }
 
-    fun convertButtonClick(view: View) {
-
-        val intent = Intent(this,ConvertActivity::class.java)
+    private fun newWindowData(istv: Boolean) {
+        val intent = Intent(this, FormatActivity::class.java)
         intent.putExtra(CONVERTER_FIRST_KEY, Bundle().apply {
-            putDouble(CONVERTER_WIDTH_PIXEL_KEY,width.toDouble())
-            putDouble(CONVERTER_HEIGHT_PIXEL_KEY,height.toDouble())
+            putDouble(CONVERTER_WIDTH_PIXEL_KEY, width.toDouble())
+            putDouble(CONVERTER_HEIGHT_PIXEL_KEY, height.toDouble())
+            putDouble(CONVERTER_WIDTH_DP_KEY, widthDP.toDouble())
+            putDouble(CONVERTER_HEIGHT_DP_KEY, heightDP.toDouble())
+            putDouble(CONVERTER_DPI, dpi.toDouble())
+            putBoolean(ISTEXTVIEW, istv)
+        })
+        startActivity(intent)
+    }
+    //endregion
+
+    //region events
+    fun convertButtonClick(view: View) {
+        val intent = Intent(this, ConvertActivity::class.java)
+        intent.putExtra(CONVERTER_FIRST_KEY, Bundle().apply {
+            putDouble(CONVERTER_WIDTH_PIXEL_KEY, width.toDouble())
+            putDouble(CONVERTER_HEIGHT_PIXEL_KEY, height.toDouble())
             putDouble(CONVERTER_WIDTH_DP_KEY, widthDP.toDouble())
             putDouble(CONVERTER_HEIGHT_DP_KEY, heightDP.toDouble())
             putDouble(CONVERTER_DPI, dpi.toDouble())
@@ -238,19 +242,5 @@ class MainActivity : AppCompatActivity() {
     fun configButtonClick(view: View) {
         newWindowData(false)
     }
-
-    private fun newWindowData(istv: Boolean)
-    {
-        val intent = Intent(this,FormatActivity::class.java)
-        intent.putExtra(CONVERTER_FIRST_KEY, Bundle().apply {
-            putDouble(CONVERTER_WIDTH_PIXEL_KEY,width.toDouble())
-            putDouble(CONVERTER_HEIGHT_PIXEL_KEY,height.toDouble())
-            putDouble(CONVERTER_WIDTH_DP_KEY, widthDP.toDouble())
-            putDouble(CONVERTER_HEIGHT_DP_KEY, heightDP.toDouble())
-            putDouble(CONVERTER_DPI, dpi.toDouble())
-            putBoolean(ISTEXTVIEW,istv)
-        })
-        startActivity(intent)
-    }
+    //endregion
 }
-

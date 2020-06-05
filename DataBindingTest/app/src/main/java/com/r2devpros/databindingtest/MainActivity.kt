deced399cb1de99ae6a633a4fb446d9d4c126bbe
Arturo S <ascstb@gmail.com>
2020-06-04 23:34:42 -0500

    Initial Commit

---
+++ DataBindingTest/app/src/main/java/com/r2devpros/databindingtest/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.r2devpros.databindingtest
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

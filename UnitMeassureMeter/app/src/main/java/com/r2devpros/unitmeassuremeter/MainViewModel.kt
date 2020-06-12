25940ec79f3bd54bcee925747fbf9eb2a6f89880
chrisalzam <chdsz123@hotmail.com>
2020-06-11 20:02:06 -0500

    Data Binding init commit

---
+++ UnitMeassureMeter/app/src/main/java/com/r2devpros/unitmeassuremeter/MainViewModel.kt
@@ -0,0 +1,20 @@
+package com.r2devpros.unitmeassuremeter
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    var StyleName: String = ""
+    var Text: String = ""
+    var TopPadding: String = ""
+    var StartPadding: String = ""
+    var EndPadding: String = ""
+    var BottomPadding: String = ""
+    var TopMargin: String = ""
+    var StartMargin: String = ""
+    var EndMargin: String = ""
+    var BottomMargin: String = ""
+    var TextSize: String = ""
+    var TextColor: String = ""
+    var BackgroundColor: String = ""
+    var StyleCode: String = ""
+}

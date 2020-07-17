dc55e784ffd10e3cb5e948c78d963292e6649d14
Christian <chdsz123@hotmail.com>
2020-07-16 23:13:14 -0500

    cleaning up code

--- AppMonitor/app/src/main/java/com/example/appmonitor/MainActivity.kt
+++ AppMonitor/app/src/main/java/com/example/appmonitor/MainActivity.kt
@@ -1,97 +1,37 @@
 package com.example.appmonitor
 
-import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
-import android.os.CountDownTimer
 import android.util.Log
 import android.view.View
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import java.util.*
 import kotlin.collections.ArrayList
-import kotlin.math.log
+import kotlin.concurrent.schedule
 
 class MainActivity : AppCompatActivity() {
     lateinit var p: ArrayList<PackageInfo>
+    private var timer = Timer("schedule", true)
+    private var startFlag = true
+    var foundApp = "com.r2devpros.unitmeassuremeter"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        p = getInstalledApps(false) ?: return
+        p = getInstalledApps() ?: return
     }
 
-    var counter = 0
-    var foundApp = "com.sentrics.engage360"
-
-    private fun startTimeCounter() {
-        val timerApp: TextView = findViewById(R.id.tvAppRunning)
-        object : CountDownTimer(500000, 5000) {
-            override fun onTick(millisUntilFinished: Long) {
-                timerApp.text = counter.toString()
-                counter++
-                Log.d("MainActivity_TAG: TimerCountdown, SECOND: ", "$counter")
-                Log.d("MainActivity_TAG: Number of INSTALLED APPS: ", "${p.size}")
-                val packageAppFound = p.firstOrNull { it.pName == foundApp } ?: return
-
-                Log.d("MainActivity_TAG", "$foundApp IS INSTALLED")
-
-                when {
-                    Helper.isAppRunning(applicationContext, packageAppFound.pName) -> {
-                        Log.d("MainActivity_TAG:", "onTick: App is already Running")
-                    }
-                    packageAppFound.pName.isNotEmpty() -> {
-                        Log.d("MainActivity_TAG: ", "LUNCHING your App")
-                        val intent = packageManager.getLaunchIntentForPackage(packageAppFound.pName)
-                        startActivity(intent)
-                    }
-                    else -> {
-                        Log.d("MainActivity_TAG: ", "Your App is NOT INSTALLED")
-                    }
-                }
-            }
-
-            override fun onFinish() {
-                timerApp.text = getString(R.string.finished_text)
-            }
-        }.start()
-    }
-
-    private var netflixId = "43598743" // <== isn't a real movie id
-    private var watchUrl = "http://www.netflix.com/watch/$netflixId"
-
-    fun onLunchButtonClicked(view: View) {
-        Log.d("MainActivity_TAG", "OnLunchButtonClick, ${view.id}")
-        try {
-            val intent = Intent(Intent.ACTION_VIEW)
-            intent.setClassName(
-                "com.netflix.mediaclient",
-                "com.netflix.mediaclient.ui.launch.UIWebViewActivity"
-            )
-            intent.data = Uri.parse(watchUrl)
-            startActivity(intent)
-        } catch (e: Exception) {
-            // netflix app isn't installed, send to website.
-            Log.d("MainActivity_TAG", "OnLunchButtonClick: Lost App")
-            val intent = Intent(Intent.ACTION_VIEW)
-            intent.data =
-                Uri.parse("https://play.google.com/store/apps/details?id=org.mozilla.firefox")
-            startActivity(intent)
-        }
-    }
-
-    private fun getInstalledApps(getSysPackages: Boolean): ArrayList<PackageInfo>? {
+    //region functions
+    private fun getInstalledApps(): ArrayList<PackageInfo>? {
         val res: ArrayList<PackageInfo> = ArrayList()
         val packs: MutableList<android.content.pm.PackageInfo> =
             packageManager.getInstalledPackages(0)
 
         for (i in packs.indices) {
-            val p = packs[i]
-            if (!getSysPackages && p.versionName == null) {
-                continue
-            }
             val newInfo = PackageInfo("", "")
-            newInfo.appName = p.applicationInfo.loadLabel(packageManager).toString()
-            newInfo.pName = p.packageName
-            val app = packageManager.getLaunchIntentForPackage(p.packageName)
+            newInfo.appName = packs[i].applicationInfo.loadLabel(packageManager).toString()
+            newInfo.pName = packs[i].packageName
+            val app = packageManager.getLaunchIntentForPackage(packs[i].packageName)
             if (app != null) {
                 Log.d("MainActivity_TAG", "packageName: ${newInfo.pName}")
                 res.add(newInfo)
@@ -100,8 +40,46 @@ class MainActivity : AppCompatActivity() {
         return res
     }
 
+
     fun onPrintPackagesButtonClicked(view: View) {
-        Log.d("MainActivity_TAG", "Clicked on Print Packages Button: ${view.id}")
-        startTimeCounter()
+        Log.d("MainActivity_TAG ", "ButtonID: ${view.id}")
+        if (startFlag) {
+            findViewById<TextView>(view.id).text = getString(R.string.btn_stop)
+            startTimeCounter()
+            startFlag = false
+        } else {
+            findViewById<TextView>(view.id).text = getString(R.string.btn_launch)
+            timer.cancel()
+            startFlag = true
+            Log.d("MainActivity_TAG","Stop Thread")
+        }
     }
+
+
+    private fun startTimeCounter() {
+        Log.d("MainActivity_TAG", "ENTERED TO STARTTIMECOUNTER2")
+        timer = Timer("schedule", true)
+        // schedule a single event
+        timer.schedule(10, 5000) {
+            //Log.d("MainActivity_TAG", "$counter2")
+            Log.d("MainActivity_TAG: Number of INSTALLED APPS: ", "${p.size}")
+            val packageAppFound = p.firstOrNull { it.pName == foundApp } ?: return@schedule
+            Log.d("MainActivity_TAG", "$foundApp IS INSTALLED")
+
+            when {
+                Helper.isAppRunning(applicationContext, packageAppFound.pName) -> {
+                    Log.d("MainActivity_TAG:", "onTick: App is already Running")
+                }
+                packageAppFound.pName.isNotEmpty() -> {
+                    Log.d("MainActivity_TAG: ", "LUNCHING your App")
+                    val intent = packageManager.getLaunchIntentForPackage(packageAppFound.pName)
+                    startActivity(intent)
+                }
+                else -> {
+                    Log.d("MainActivity_TAG: ", "Your App is NOT INSTALLED")
+                }
+            }
+        }
+    }
+    //endregion
 }

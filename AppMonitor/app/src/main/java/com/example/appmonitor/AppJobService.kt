dad0f25c73b9f3cc1fba5d0e1e002c0038dfb19c
Christian <chdsz123@hotmail.com>
2020-07-21 00:19:52 -0500

    Job service implementation

---
+++ AppMonitor/app/src/main/java/com/example/appmonitor/AppJobService.kt
@@ -0,0 +1,86 @@
+package com.example.appmonitor
+
+import android.app.job.JobParameters
+import android.app.job.JobService
+import android.util.Log
+import java.util.*
+import java.util.logging.Handler
+import kotlin.collections.ArrayList
+
+class AppJobService : JobService() {
+    var looking4App = "com.r2devpros.unitmeassuremeter"
+    private val TAG = "AppJobService_TAG: "
+    var jobCancelled = false
+
+    override fun onStartJob(params: JobParameters?): Boolean {
+        Log.d(TAG, "Job Started")
+        findInstalledRunningApp(params)
+        return true
+    }
+
+    override fun onStopJob(params: JobParameters?): Boolean {
+        Log.d(TAG, "Job cancelled before completion")
+        jobCancelled = true
+        return false
+    }
+
+
+
+    private fun findInstalledRunningApp(params: JobParameters?) {
+        val p = getInstalledApps() ?: return
+        var notInstalled : Boolean
+
+//        val timer = Timer()
+//        val monitor = object : TimerTask() {
+//            override fun run() {
+                Log.d("$TAG: Number of INSTALLED APPS: ", "${p.size}")
+                for (i in 1..5) {
+                    Log.d(TAG,"Second $i")
+                    if (jobCancelled) {
+                        return
+                    }
+
+                    val packageAppFound = p.firstOrNull { it.pName == looking4App } ?: return
+                    notInstalled = false
+                    Log.d(TAG, "$looking4App IS INSTALLED")
+                    when {
+                        Helper.isAppRunning(applicationContext, packageAppFound.pName) -> {
+                            Log.d(TAG, "App is already Running")
+                        }
+                        packageAppFound.pName.isNotEmpty() -> {
+                            Log.d(TAG, "LUNCHING your App")
+                            val intent =
+                                packageManager.getLaunchIntentForPackage(packageAppFound.pName)
+                            startActivity(intent)
+                        }
+                    }
+                    if (notInstalled)
+                        Log.d(TAG, "Your App IS NOT INSTALLED")
+                }
+                Log.d(TAG, "Job Finished")
+                jobFinished(params, false)
+//            }
+//        }
+//        timer.schedule(monitor, 1000, 1000)
+    }
+
+    private fun getInstalledApps(): ArrayList<PackageInfo>? {
+        val res: ArrayList<PackageInfo> = ArrayList()
+        val packs: MutableList<android.content.pm.PackageInfo> =
+            packageManager.getInstalledPackages(0)
+
+        for (i in packs.indices) {
+            val newInfo = PackageInfo("", "")
+            newInfo.appName = packs[i].applicationInfo.loadLabel(packageManager).toString()
+            newInfo.pName = packs[i].packageName
+            val app = packageManager.getLaunchIntentForPackage(packs[i].packageName)
+            if (app != null) {
+                Log.d(TAG, "packageName: ${newInfo.pName}")
+                res.add(newInfo)
+            }
+        }
+        return res
+    }
+
+
+}
--- AppMonitor/app/src/main/java/com/example/appmonitor/MainActivity.kt
+++ AppMonitor/app/src/main/java/com/example/appmonitor/MainActivity.kt
@@ -1,85 +1,115 @@
 package com.example.appmonitor
 
+import android.app.job.JobInfo
+import android.app.job.JobScheduler
+import android.content.ComponentName
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentActivity
 import java.util.*
 import kotlin.collections.ArrayList
-import kotlin.concurrent.schedule
+
 
 class MainActivity : AppCompatActivity() {
     lateinit var p: ArrayList<PackageInfo>
     private var timer = Timer("schedule", true)
     private var startFlag = true
     var foundApp = "com.r2devpros.unitmeassuremeter"
+    val TAG = "MainActivity_TAG "
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        p = getInstalledApps() ?: return
+//        p = getInstalledApps() ?: return
+
     }
 
     //region functions
-    private fun getInstalledApps(): ArrayList<PackageInfo>? {
-        val res: ArrayList<PackageInfo> = ArrayList()
-        val packs: MutableList<android.content.pm.PackageInfo> =
-            packageManager.getInstalledPackages(0)
-
-        for (i in packs.indices) {
-            val newInfo = PackageInfo("", "")
-            newInfo.appName = packs[i].applicationInfo.loadLabel(packageManager).toString()
-            newInfo.pName = packs[i].packageName
-            val app = packageManager.getLaunchIntentForPackage(packs[i].packageName)
-            if (app != null) {
-                Log.d("MainActivity_TAG", "packageName: ${newInfo.pName}")
-                res.add(newInfo)
-            }
-        }
-        return res
-    }
-
 
     fun onPrintPackagesButtonClicked(view: View) {
-        Log.d("MainActivity_TAG ", "ButtonID: ${view.id}")
+        Log.d(TAG, "ButtonID: ${view.id}")
         if (startFlag) {
             findViewById<TextView>(view.id).text = getString(R.string.btn_stop)
-            startTimeCounter()
-            startFlag = false
-        } else {
-            findViewById<TextView>(view.id).text = getString(R.string.btn_launch)
-            timer.cancel()
-            startFlag = true
-            Log.d("MainActivity_TAG","Stop Thread")
-        }
-    }
+            val componentName = ComponentName(this, AppJobService::class.java)
+            val info = JobInfo.Builder(123, componentName)
+                //.setRequiresCharging(true)
+                //.setRequiredNetworkType(JobInfo.NETWORK_TYPE_UNMETERED)
+                .setPersisted(true)
+                .setPeriodic(1 * 60 * 1000)
+                .build()
 
+            val scheduler =
+                getSystemService(Context.JOB_SCHEDULER_SERVICE) as JobScheduler
+            val resultCode = scheduler.schedule(info)
 
-    private fun startTimeCounter() {
-        Log.d("MainActivity_TAG", "ENTERED TO STARTTIMECOUNTER2")
-        timer = Timer("schedule", true)
-        // schedule a single event
-        timer.schedule(10, 5000) {
-            //Log.d("MainActivity_TAG", "$counter2")
-            Log.d("MainActivity_TAG: Number of INSTALLED APPS: ", "${p.size}")
-            val packageAppFound = p.firstOrNull { it.pName == foundApp } ?: return@schedule
-            Log.d("MainActivity_TAG", "$foundApp IS INSTALLED")
-
-            when {
-                Helper.isAppRunning(applicationContext, packageAppFound.pName) -> {
-                    Log.d("MainActivity_TAG:", "onTick: App is already Running")
-                }
-                packageAppFound.pName.isNotEmpty() -> {
-                    Log.d("MainActivity_TAG: ", "LUNCHING your App")
-                    val intent = packageManager.getLaunchIntentForPackage(packageAppFound.pName)
-                    startActivity(intent)
-                }
-                else -> {
-                    Log.d("MainActivity_TAG: ", "Your App is NOT INSTALLED")
-                }
+            if (resultCode == JobScheduler.RESULT_SUCCESS) {
+                Log.d(TAG, "Job Scheduled")
+            } else {
+                Log.d(TAG, "Job Scheduling fail")
             }
+            startFlag = false
+        }
+        else
+        {
+            findViewById<TextView>(view.id).text = getString(R.string.btn_launch)
+            val scheduler = getSystemService(Context.JOB_SCHEDULER_SERVICE) as JobScheduler
+            scheduler.cancel(123)
+            Log.d(TAG, "Job cancelled")
+            startFlag = true
         }
     }
+
+    //    private fun getInstalledApps(): ArrayList<PackageInfo>? {
+//        val res: ArrayList<PackageInfo> = ArrayList()
+//        val packs: MutableList<android.content.pm.PackageInfo> =
+//            packageManager.getInstalledPackages(0)
+//
+//        for (i in packs.indices) {
+//            val newInfo = PackageInfo("", "")
+//            newInfo.appName = packs[i].applicationInfo.loadLabel(packageManager).toString()
+//            newInfo.pName = packs[i].packageName
+//            val app = packageManager.getLaunchIntentForPackage(packs[i].packageName)
+//            if (app != null) {
+//                Log.d("MainActivity_TAG", "packageName: ${newInfo.pName}")
+//                res.add(newInfo)
+//            }
+//        }
+//        return res
+//    }
+
+//    private fun startTimeCounter() {
+//        Log.d("MainActivity_TAG", "ENTERED TO STARTTIMECOUNTER2")
+//        timer = Timer("schedule", true)
+//        // schedule a single event
+//        timer.schedule(10, 5000) {
+//            //Log.d("MainActivity_TAG", "$counter2")
+//            Log.d("MainActivity_TAG: Number of INSTALLED APPS: ", "${p.size}")
+//            val packageAppFound = p.firstOrNull { it.pName == foundApp } ?: return@schedule
+//            Log.d("MainActivity_TAG", "$foundApp IS INSTALLED")
+//
+//            when {
+//                Helper.isAppRunning(applicationContext, packageAppFound.pName) -> {
+//                    Log.d("MainActivity_TAG:", "onTick: App is already Running")
+//                }
+//                packageAppFound.pName.isNotEmpty() -> {
+//                    Log.d("MainActivity_TAG: ", "LUNCHING your App")
+//                    val intent = packageManager.getLaunchIntentForPackage(packageAppFound.pName)
+//                    startActivity(intent)
+//                }
+//                else -> {
+//                    Log.d("MainActivity_TAG: ", "Your App is NOT INSTALLED")
+//                }
+//            }
+//        }
+//    }
+
+
     //endregion
+
+    //CREA UN SERVICIO QUE SE EJECUTE CADA 15 MINUTOS
+    //THREAD.SLEEP CADA 15 MINUTOS, QUE SE ESPERE 5 SEGUNDOS Y GENERE UN LOG
 }
